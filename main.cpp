f450786e1ae27ca246763a290bb4d0608061868f
Kingsley Anyichie <111355135+Kingston7777777@users.noreply.github.com>
2022-11-07 01:39:57 -0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Kingsley has started his C++ software engineering course" <<std::endl;
+    std::cout << "C++ is my best programming language";
+    return 0;
+}
